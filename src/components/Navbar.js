87d0fd505226f9f805a5b6f3c68107181446f73d
mattneary1 <mattneary1@gmail.com>
2022-10-27 17:17:11 -0700

    Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import Logo from "../images/logo.png";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+	const [click, setClick] = useState(false);
+	const handleClick = () => setClick(!click);
+
 	return (
 		<div className="navbar">
 			<div className="logo">
 				<img src={Logo} alt="logo" />
 			</div>
-			<ul className="nav-menu">
+			<ul className={click ? 'nav-menu active' : 'nav-menu'}>
 				<li className="nav-item">
 					<a href="/">Home</a>
 				</li>
@@ -23,8 +26,13 @@ const Navbar = () => {
 					<a href="/">Contact</a>
 				</li>
 			</ul>
-			<div className="hamburger">
-				<FaBars />
+			<div className="hamburger" onClick={handleClick}>
+				{click ? (
+					<FaTimes size={30} style={{ color: "#f8f8f8" }} />
+				) : (
+					<FaBars size={30} style={{ color: "#f8f8f8" }} />
+				)}
+				/>
 			</div>
 		</div>
 	);
